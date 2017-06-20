4e839051006ddbadcc43bde32a8374736ebc5658
Rockford Wei <rocky@perfect.org>
2017-06-20 12:55:49 -0400

    Removing image size upload restriction

--- Sources/main.swift
+++ Sources/main.swift
@@ -27,28 +27,24 @@ typealias TF = TensorFlow
 
 
 class LabelImage {
-  let g: TF.Graph
-
-  public init() throws {
-    g = try TF.Graph()
-  }
+  let def: TF.GraphDef
 
   public init(_ model:Data) throws {
-    g = try TF.Graph()
-    let def = try TF.GraphDef(serializedData: model)
-    try g.import(definition: def)
+    def = try TF.GraphDef(serializedData: model)
   }
 
   public func match(image: Data) throws -> (Int, Int) {
-    let normalized = try constructAndExecuteGraphToNormalizeImage(imageBytes: image)
-    let possibilities = try executeInceptionGraph(image: normalized)
+    let g = try TF.Graph()
+    try g.import(definition: def)
+    let normalized = try constructAndExecuteGraphToNormalizeImage(g, imageBytes: image)
+    let possibilities = try executeInceptionGraph(g, image: normalized)
     guard let m = possibilities.max(), let i = possibilities.index(of: m) else {
       throw TF.Panic.INVALID
     }//end guard
     return (i, Int(m * 100))
   }
 
-  private func executeInceptionGraph(image: TF.Tensor) throws -> [Float] {
+  private func executeInceptionGraph(_ g: TF.Graph, image: TF.Tensor) throws -> [Float] {
     let results = try g.runner().feed("input", tensor: image).fetch("output").run()
     guard results.count > 0 else { throw TF.Panic.INVALID }
     let result = results[0]
@@ -58,7 +54,7 @@ class LabelImage {
     let res: [Float] = try result.asArray()
     return res
   }//end exec
-  public func constructAndExecuteGraphToNormalizeImage(imageBytes: Data) throws -> TF.Tensor {
+  public func constructAndExecuteGraphToNormalizeImage(_ g: TF.Graph, imageBytes: Data) throws -> TF.Tensor {
     let H:Int32 = 224
     let W:Int32 = 224
     let mean:Float = 117
@@ -80,9 +76,8 @@ class LabelImage {
   }//end normalize
 }
 
-var inceptionModel: Data? = nil
+var inceptionModel: LabelImage? = nil
 var tags = [String]()
-
 
 func handler(data: [String:Any]) throws -> RequestHandler {
   return {
@@ -90,7 +85,7 @@ func handler(data: [String:Any]) throws -> RequestHandler {
     let prefix = "data:image/jpeg;base64,"
     guard let input = request.postBodyString, input.hasPrefix(prefix),
     let b64 = String(input.utf8.dropFirst(prefix.utf8.count)),
-    let model = inceptionModel
+    let scanner = inceptionModel
     else {
       response.setHeader(.contentType, value: "text/json")
         .appendBody(string: "{\"value\": \"Invalid Input\"}")
@@ -106,7 +101,6 @@ func handler(data: [String:Any]) throws -> RequestHandler {
     }//end guard
 
     do {
-      let scanner = try LabelImage(model)
       let result = try scanner.match(image: data)
       guard result.0 > -1 && result.0 < tags.count else {
         response.setHeader(.contentType, value: "text/json")
@@ -170,7 +164,7 @@ do {
   let lines = try fTag.readString()
   tags = lines.utf8.split(separator: 10).map { String(describing: $0) }
   try TF.Open()
-  inceptionModel = Data(bytes: modelBytes)
+  inceptionModel = try LabelImage( Data(bytes: modelBytes) )
   print("library ready")
   try HTTPServer.launch(configurationData: confData)
  }catch {
